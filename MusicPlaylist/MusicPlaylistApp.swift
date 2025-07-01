f10f6ae25b0aa35b6db5f6fea4fee9e047255d5a
Reza <rezafh19@gmail.com>
2025-07-01 13:30:31 +0700

    chore: initial commit

---
+++ MusicPlaylist/MusicPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlaylistApp.swift
+//  MusicPlaylist
+//
+//  Created by Reza Harris on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
